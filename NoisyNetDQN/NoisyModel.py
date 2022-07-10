94f5970c5324ac51dcbc44dac009c936c1a11bc3
Tiago <tiagonegri237@gmail.com>
2022-07-10 14:43:39 -0300

    Noisy DQN implemented

---
+++ NoisyNetDQN/NoisyModel.py
@@ -0,0 +1,56 @@
+import torch as th
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchinfo
+from NoisyNetDQN.NoisyLinear import NoisyLinear, NoisyFactorizedLinear
+
+class NoisyModel(nn.Module):
+
+    def __init__(self, input_dim, action_dim, learning_rate, sigma_init, device) -> None:
+        super(NoisyModel, self).__init__()
+        self.action_dim = action_dim
+        self.learning_rate = learning_rate
+        self.device = device
+        self.input_dim = input_dim
+        self.sigma_init = sigma_init
+
+        arch = {'net_arch':[24, 24], 'activation_fn':nn.ReLU}
+        self.q_net = self.make_network(arch, input_dim, action_dim).to(device)
+
+        self.target_net = self.make_network(arch, input_dim, action_dim).to(device)
+
+        self.loss_func = nn.MSELoss(reduction='none')
+        self.optimizer = optim.Adam(self.q_net.parameters(), lr=self.learning_rate)
+
+    def make_network(self, achitecture, input_dim, output_dim) -> nn.Sequential:
+        activation = achitecture['activation_fn']
+        net_arch = achitecture['net_arch']
+
+        net = nn.Sequential()
+        last_dim = input_dim[0]
+        for i in range(len(net_arch)):
+            net.add_module("layer_{}".format(i+1), NoisyFactorizedLinear(last_dim, net_arch[i], self.sigma_init, bias=True))
+            net.add_module("activation_{}".format(i+1), activation())
+            last_dim = net_arch[i]
+        net.add_module("ouput", NoisyFactorizedLinear(last_dim, output_dim, self.sigma_init, bias=True))
+        return net
+
+    def reset_noise(self):
+        # for m in self.q_net.modules():
+        #     if isinstance(m, NoisyLinear):
+        #         m.reset_noise()
+        pass
+
+    def __str__(self) -> str:
+        return torchinfo.summary(self.q_net, self.input_dim, device=self.device).__str__()
+
+    def q_values(self, state):
+        return self.q_net(state)
+
+    def q_target(self, state):
+        return self.target_net(state)
+
+    def sync(self):
+        print("Sync target network...")
+        self.target_net.load_state_dict(self.q_net.state_dict())
---
+++ NoisyNetDQN/NoisyNetDQNAgent.py
@@ -0,0 +1,44 @@
+from DQN.DQNAgent import DQNAgent
+from NoisyNetDQN.NoisyModel import NoisyModel
+import torch as th
+import numpy as np
+
+class NoisyNetDQNAgent(DQNAgent):
+
+    def __init__(self, 
+                    input_dim, 
+                    action_dim, 
+                    initial_epsilon, 
+                    final_epsilon, 
+                    epsilon_decay, 
+                    learning_rate, 
+                    gamma, 
+                    batch_size, 
+                    experience_buffer_size, 
+                    target_network_sync_freq,
+                    sigma_init,
+                    device='cpu'
+                ):
+                
+        super().__init__(input_dim, action_dim, initial_epsilon, final_epsilon, epsilon_decay, learning_rate, gamma, batch_size, experience_buffer_size, target_network_sync_freq, device)
+        self.model = NoisyModel(input_dim, action_dim, learning_rate, sigma_init, device)
+        self.epsilon = 0
+        self.final_epsilon = 0
+        # print(self.model)
+
+    # def step(self):
+    #     super().step()
+    #     self.model.reset_noise()
+
+    @th.no_grad()
+    def getAction(self, state, mask=None, deterministic=False):
+        self.model.train(True)
+        if mask is None:
+            mask = np.ones(self.action_dim, dtype=np.bool)
+
+        with th.no_grad():
+            mask = np.invert(mask)
+            state = th.tensor(state, dtype=th.float).to(self.model.device)
+            q_values = self.model.q_values(state)
+            q_values[mask] = -th.inf
+            return q_values.argmax().item()
