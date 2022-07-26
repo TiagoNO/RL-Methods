1d405a024d82ca5d85ac86e178099cc58e62564a
Tiago <tiagonegri237@gmail.com>
2022-07-26 12:57:16 -0300

    Script that runs every DQN methods implemented

--- teste.py
+++ teste.py
@@ -30,39 +30,62 @@ def generate_graph(filename, scores, plot_every=10):
 if __name__ == '__main__':
     file_name = "montecarloagent.pk"
 
-    n_episodes = 20000
+    n_episodes = 5000
     plot_every = 100
     debug_every = 100
     env_name = "CartPole-v0"
-    # env_name = "LunarLander-v2"
+    
+    # DQN models parameters
+    initial_epsilon=1.0
+    final_epsilon=0.05
+    epsilon_decay=1e-5
+    learning_rate=.001
+    gamma=.9
+    batch_size=64
+    experience_buffer_size=1e6
+    target_network_sync_freq=2000
 
-    # env = gym.make(env_name)
-    # qAgent = QLearningAgent(env.action_space.n, lr=.5, discount=.99, initial_epsilon=1, final_epsilon=.1, epsilon_decay=1e-5)
-    # qAgent.train(env, n_episodes)
-    # generate_graph("tabular_qlearning", qAgent.scores, plot_every)
+    #prioritized buffer parameters
+    experience_prob_alpha=0.6
+    experience_beta=0.4
+    experience_beta_decay=1e-5
 
-    # env = gym.make(env_name)
-    # doubleQAgent = DoubleQLearningAgent(env.action_space.n, lr=.5, discount=.99, initial_epsilon=1, final_epsilon=.1 ,epsilon_decay=1e-5)
-    # dq_scores = doubleQAgent.train(env, n_episodes)
-    # generate_graph("tabular_double_qlearning", dq_scores, plot_every)
+    # multi-step parameters
+    trajectory_steps=4
+
+    # Noisy parameters
+    initial_sigma=.9
+
+    # Distributional parameters
+    n_atoms=2
+    min_value=1
+    max_value=200
+
+    # Common parameters
+    checkpoint_freq=50000
+    savedir="experiments/rainbow/"
+    log_freq=1
+    arch={'net_arch':[24, 24, 24], 'activation_fn':th.nn.ReLU}
+    dueling_arch={'feature_arch':[24, 24, 24], 'value_arch':[24, 12], 'advantage_arch':[24, 12], 'activation_fn':th.nn.ReLU}
+    device="cpu"
 
     env = gym.make(env_name)
     dqnAgent = DQNAgent(
                         env.observation_space.shape,
                         env.action_space.n,
-                        initial_epsilon=1.0,
-                        final_epsilon=0.1,
-                        epsilon_decay=1e-5,
-                        learning_rate=.001,
-                        gamma=.9,
-                        batch_size=64,
-                        experience_buffer_size=1e5,
-                        target_network_sync_freq=3000,
-                        checkpoint_freq=50000,
+                        initial_epsilon=initial_epsilon,
+                        final_epsilon=final_epsilon,
+                        epsilon_decay=epsilon_decay,
+                        learning_rate=learning_rate,
+                        gamma=gamma,
+                        batch_size=batch_size,
+                        experience_buffer_size=experience_buffer_size,
+                        target_network_sync_freq=target_network_sync_freq,
+                        checkpoint_freq=checkpoint_freq,
                         savedir="experiments/dqn/",
-                        log_freq=1,
-                        architecture={'net_arch':[24, 24], 'activation_fn':th.nn.ReLU},
-                        device='cpu'
+                        log_freq=log_freq,
+                        architecture=arch,
+                        device=device,
                             )
     dqnAgent.train(env, n_episodes)
     generate_graph(dqnAgent.savedir + "scores", dqnAgent.scores, plot_every)
@@ -73,19 +96,19 @@ if __name__ == '__main__':
     doubledqnAgent = DoubleDQNAgent(
                         env.observation_space.shape,
                         env.action_space.n,
-                        initial_epsilon=1.0,
-                        final_epsilon=0.05,
-                        epsilon_decay=1e-5,
-                        learning_rate=.001,
-                        gamma=.9,
-                        batch_size=32,
-                        experience_buffer_size=1e6,
-                        target_network_sync_freq=2000,
-                        checkpoint_freq=50000,
+                        initial_epsilon=initial_epsilon,
+                        final_epsilon=final_epsilon,
+                        epsilon_decay=epsilon_decay,
+                        learning_rate=learning_rate,
+                        gamma=gamma,
+                        batch_size=batch_size,
+                        experience_buffer_size=experience_buffer_size,
+                        target_network_sync_freq=target_network_sync_freq,
+                        checkpoint_freq=checkpoint_freq,
                         savedir="experiments/double_dqn/",
-                        log_freq=1,
-                        architecture={'net_arch':[24, 24], 'activation_fn':th.nn.ReLU},
-                        device='cpu'
+                        log_freq=log_freq,
+                        architecture=arch,
+                        device=device,
                             )
     doubledqnAgent.train(env, n_episodes)
     generate_graph(doubledqnAgent.savedir + "scores", doubledqnAgent.scores, plot_every)
@@ -96,22 +119,22 @@ if __name__ == '__main__':
     prioritizedDQNAgent = PrioritizedDQN(
                         env.observation_space.shape,
                         env.action_space.n,
-                        initial_epsilon=1.0,
-                        final_epsilon=0.05,
-                        epsilon_decay=1e-5,
-                        learning_rate=.01,
-                        gamma=.9,
-                        batch_size=32,
-                        experience_buffer_size=1e6,
-                        target_network_sync_freq=2000,
-                        experience_prob_alpha=0.6,
-                        experience_beta=0.4,
-                        experience_beta_decay=1e-6,
-                        checkpoint_freq=50000,
+                        initial_epsilon=initial_epsilon,
+                        final_epsilon=final_epsilon,
+                        epsilon_decay=epsilon_decay,
+                        learning_rate=learning_rate,
+                        gamma=gamma,
+                        batch_size=batch_size,
+                        experience_buffer_size=experience_buffer_size,
+                        target_network_sync_freq=target_network_sync_freq,
+                        experience_prob_alpha=experience_prob_alpha,
+                        experience_beta=experience_beta,
+                        experience_beta_decay=experience_beta_decay,
+                        checkpoint_freq=checkpoint_freq,
                         savedir="experiments/prioritized/",
-                        log_freq=1,
-                        architecture={'net_arch':[24, 24], 'activation_fn':th.nn.ReLU},
-                        device='cpu'
+                        log_freq=log_freq,
+                        architecture=arch,
+                        device=device,
                         )
     prioritizedDQNAgent.train(env, n_episodes)
     generate_graph(prioritizedDQNAgent.savedir + "scores", prioritizedDQNAgent.scores, plot_every)
@@ -122,19 +145,19 @@ if __name__ == '__main__':
     duelingDQNAgent = DuelingDQNAgent(
                         env.observation_space.shape,
                         env.action_space.n,
-                        initial_epsilon=1.0,
-                        final_epsilon=0.05,
-                        epsilon_decay=1e-5,
-                        learning_rate=.01,
-                        gamma=.9,
-                        batch_size=32,
-                        experience_buffer_size=1e6,
-                        target_network_sync_freq=2000,
-                        checkpoint_freq=50000,
-                        savedir="",
-                        log_freq=1,
-                        architecture={'feature_arch':[24, 24], 'value_arch':[24, 12], 'advantage_arch':[24, 12], 'activation_fn':th.nn.ReLU},
-                        device='cpu'
+                        initial_epsilon=initial_epsilon,
+                        final_epsilon=final_epsilon,
+                        epsilon_decay=epsilon_decay,
+                        learning_rate=learning_rate,
+                        gamma=gamma,
+                        batch_size=batch_size,
+                        experience_buffer_size=experience_buffer_size,
+                        target_network_sync_freq=target_network_sync_freq,
+                        checkpoint_freq=checkpoint_freq,
+                        savedir="experiments/dueling/",
+                        log_freq=log_freq,
+                        architecture=dueling_arch,
+                        device=device,
                         )
     duelingDQNAgent.train(env, n_episodes)
     generate_graph(duelingDQNAgent.savedir + "scores", duelingDQNAgent.scores, plot_every)
@@ -145,20 +168,20 @@ if __name__ == '__main__':
     multistepDQNAgent = MultiStepDQNAgent(
                         env.observation_space.shape,
                         env.action_space.n,
-                        initial_epsilon=1.0,
-                        final_epsilon=0.05,
-                        epsilon_decay=1e-5,
-                        learning_rate=.01,
-                        gamma=.9,
-                        batch_size=32,
-                        experience_buffer_size=1e6,
-                        target_network_sync_freq=2000,
-                        trajectory_steps=4,
-                        checkpoint_freq=50000,
+                        initial_epsilon=initial_epsilon,
+                        final_epsilon=final_epsilon,
+                        epsilon_decay=epsilon_decay,
+                        learning_rate=learning_rate,
+                        gamma=gamma,
+                        batch_size=batch_size,
+                        experience_buffer_size=experience_buffer_size,
+                        target_network_sync_freq=target_network_sync_freq,
+                        trajectory_steps=trajectory_steps,
+                        checkpoint_freq=checkpoint_freq,
                         savedir="experiments/multistep/",
-                        log_freq=1,
-                        architecture={'net_arch':[24, 24], 'activation_fn':th.nn.ReLU},
-                        device='cpu'
+                        log_freq=log_freq,
+                        architecture=arch,
+                        device=device,
                         )
     multistepDQNAgent.train(env, n_episodes)
     generate_graph(multistepDQNAgent.savedir + "scores", multistepDQNAgent.scores, plot_every)
@@ -167,20 +190,20 @@ if __name__ == '__main__':
     noisyDQNAgent = NoisyNetDQNAgent(
                         env.observation_space.shape,
                         env.action_space.n,
-                        initial_epsilon=1.0,
-                        final_epsilon=0.05,
-                        epsilon_decay=1e-5,
-                        learning_rate=.01,
-                        gamma=.9,
-                        batch_size=32,
-                        experience_buffer_size=1e6,
-                        target_network_sync_freq=2000,
-                        sigma_init=.9,
-                        checkpoint_freq=50000,
+                        initial_epsilon=initial_epsilon,
+                        final_epsilon=final_epsilon,
+                        epsilon_decay=epsilon_decay,
+                        learning_rate=learning_rate,
+                        gamma=gamma,
+                        batch_size=batch_size,
+                        experience_buffer_size=experience_buffer_size,
+                        target_network_sync_freq=target_network_sync_freq,
+                        sigma_init=initial_sigma,
+                        checkpoint_freq=checkpoint_freq,
                         savedir="experiments/noisy/",
-                        log_freq=1,
-                        architecture={'net_arch':[24, 24], 'activation_fn':th.nn.ReLU},
-                        device='cpu'
+                        log_freq=log_freq,
+                        architecture=arch,
+                        device=device,
                         )
     noisyDQNAgent.train(env, n_episodes)
     generate_graph(noisyDQNAgent.savedir + "scores", noisyDQNAgent.scores, plot_every)
@@ -191,22 +214,22 @@ if __name__ == '__main__':
     distributionalDQNAgent = DistributionalDQNAgent(
                         env.observation_space.shape,
                         env.action_space.n,
-                        initial_epsilon=1.0,
-                        final_epsilon=0.05,
-                        epsilon_decay=1e-5,
-                        learning_rate=0.001,
-                        gamma=.9,
-                        batch_size=64,
-                        experience_buffer_size=1e6,
-                        target_network_sync_freq=2000,
-                        n_atoms=51,
-                        min_value=1,
-                        max_value=200,
-                        checkpoint_freq=50000,
+                        initial_epsilon=initial_epsilon,
+                        final_epsilon=final_epsilon,
+                        epsilon_decay=epsilon_decay,
+                        learning_rate=learning_rate,
+                        gamma=gamma,
+                        batch_size=batch_size,
+                        experience_buffer_size=experience_buffer_size,
+                        target_network_sync_freq=target_network_sync_freq,
+                        n_atoms=n_atoms,
+                        min_value=min_value,
+                        max_value=max_value,
+                        checkpoint_freq=checkpoint_freq,
                         savedir="experiments/distributional/",
-                        log_freq=1,
-                        architecture={'net_arch':[24, 24], 'activation_fn':th.nn.ReLU},
-                        device='cpu'
+                        log_freq=log_freq,
+                        architecture=arch,
+                        device=device,
                         )
     distributionalDQNAgent.train(env, n_episodes)
     generate_graph(distributionalDQNAgent.savedir + "scores", distributionalDQNAgent.scores, plot_every)
@@ -217,27 +240,27 @@ if __name__ == '__main__':
     rainbowAgent = RainbowAgent(
                         env.observation_space.shape,
                         env.action_space.n,
-                        initial_epsilon=1.0,
-                        final_epsilon=0.05,
-                        epsilon_decay=1e-5,
-                        learning_rate=.0001,
-                        gamma=.9,
-                        batch_size=32,
-                        experience_buffer_size=1e6,
-                        target_network_sync_freq=2000,
-                        experience_prob_alpha=0.6,
-                        experience_beta=0.4,
-                        experience_beta_decay=1e-6,
-                        trajectory_steps=4,
-                        initial_sigma=.9,
-                        n_atoms=2,
-                        min_value=1,
-                        max_value=200,
-                        checkpoint_freq=10000,
+                        initial_epsilon=initial_epsilon,
+                        final_epsilon=final_epsilon,
+                        epsilon_decay=epsilon_decay,
+                        learning_rate=learning_rate,
+                        gamma=gamma,
+                        batch_size=batch_size,
+                        experience_buffer_size=experience_buffer_size,
+                        target_network_sync_freq=target_network_sync_freq,
+                        experience_prob_alpha=experience_prob_alpha,
+                        experience_beta=experience_beta,
+                        experience_beta_decay=experience_beta_decay,
+                        trajectory_steps=trajectory_steps,
+                        initial_sigma=initial_sigma,
+                        n_atoms=n_atoms,
+                        min_value=min_value,
+                        max_value=max_value,
+                        checkpoint_freq=checkpoint_freq,
                         savedir="experiments/rainbow/",
-                        log_freq=1,
-                        architecture={'feature_arch':[24, 24], 'value_arch':[24, 12], 'advantage_arch':[24, 12], 'activation_fn':th.nn.ReLU},
-                        device='cpu'
+                        log_freq=log_freq,
+                        architecture=dueling_arch,
+                        device=device,
                         )
     rainbowAgent.train(env, n_episodes)
     generate_graph(rainbowAgent.savedir + "scores", rainbowAgent.scores, plot_every)
