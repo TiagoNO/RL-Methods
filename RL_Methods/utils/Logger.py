683b1c149c82029b3a2e5bce486cc2d41519e252
Tiago <tiagonegri237@gmail.com>
2022-08-01 12:39:27 -0300

    Now logger dump data in file and no longer stores in memory RAM

--- RL_Methods/utils/Logger.py
+++ RL_Methods/utils/Logger.py
@@ -11,15 +11,11 @@ class Logger:
         if not os.path.isdir(self.directory):
             os.makedirs(self.directory)
 
-        if os.path.isfile(self.directory + self.filename):
-            self.load()
-        else:
-            self.data = {}
+        self.data = {}
 
     def log(self, name, value):
         name_keys = name.split("/")
         values = self.data
-        # value = round(value, 8)
         for key in name_keys:
             if not key in values:
                 values[key] = {}
@@ -29,11 +25,56 @@ class Logger:
             values['data'] = []
         values['data'].append(value)
 
+    def merge(self, values, target):
+        print(values, target)
+        if 'data' in target:
+            if 'data' in values:
+                values['data'].extend(target['data'])
+            else:
+                values['data'] = target['data']
+            return
+
+        for key in target.keys():
+            if not key in values:
+                values[key] = {}
+            self.merge(values[key], target[key])
+        return values
+
     def dump(self):
+        loaded_data = self.retrieve()
+        self.data = self.merge(loaded_data, self.data)
         with open(self.directory + self.filename, 'w') as f:
             json.dump(self.data, f)
+        self.data.clear()
 
-    def load(self):
-        with open(self.directory + self.filename, 'r') as f:
-            self.data = json.load(f)
+    def retrieve(self, filename=None):
+        if filename is None:
+            filename = self.directory + self.filename
 
+        try:
+            with open(filename, 'r') as f:
+                return json.load(f)
+        except:
+            return {}
+
+    def load(self, filename=None):
+        self.data = self.retrieve(filename)
+
+# logger = Logger("experiments/logger/teste")
+# logger.log("parameters/alpha", 10)
+# logger.log("parameters/beta", 1)
+# logger.dump()
+# input()
+# logger.log("parameters/alpha", 11)
+# logger.log("parameters/alpha", 12)
+# logger.log("parameters/alpha", 13)
+# logger.log("parameters/gamma", 0.1)
+# logger.dump()
+# input()
+# del logger
+
+# logger = Logger("experiments/logger/teste")
+# logger.log("parameters/gamma", 0.01)
+# logger.log("parameters/gamma", 0.001)
+# logger.dump()
+# input()
